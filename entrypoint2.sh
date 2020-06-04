3ef0de79bd4c3d7fb5ca6e20c9069464f105450f
Samuel Melrose <sam.melrose@a1comms.com>
2020-06-04 11:11:13 +0100

    cisco log

--- entrypoint2.sh
+++ entrypoint2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed 's#%%NETWORK_NAME%%#'"$NETWORK_NAME"'#' -i /etc/google-fluentd/config.d/netsyslog.conf
+sed 's#%%NETWORK_NAME%%#'"$NETWORK_NAME"'#g' -i /etc/google-fluentd/config.d/netsyslog.conf
 
 exec "/entrypoint.sh" "$@"
